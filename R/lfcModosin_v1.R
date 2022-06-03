ae4143dbb7f7442b69fc934f90daed2fc4d77857
Oleguer Amorós Mèlich <o.amoros@creaf.uab.es>
2022-06-03 13:30:07 +0200

    class LFC_MODOSIN create

---
+++ R/lfcModosin_v1.R
@@ -0,0 +1,45 @@
+
+
+modosin <- function() {
+  lfcMODOSIN$new()
+}
+
+lfcMODOSIN <- R6::R6Class(
+
+  classname = "lfcMODOSIN",
+  cloneable = FALSE,
+
+  public = list(
+    val_1 = NA,
+    val_2 = NA,
+    res = NA,
+    add = function(a,b){
+      check_values(a,b)
+      self$val_1 <- a
+      self$val_2 <- b
+    },
+    url = function(){
+      cat(
+        "URL de GitHub = ",
+        crayon::blue$underline("https://github.com/OleAmo/prova_LFCDATA)\n")
+      )
+    },
+    sum = function(){
+      res <- self$val_1 + self$val_2
+
+      cat(
+        "SUMA = " %+% crayon::yellow$bold(paste(self$val_1))%+% " + " %+%
+          crayon::yellow$bold(paste(self$val_2)) %+%
+          "\n")
+      if (res>=0) {
+        cat("RESULTADO = " %+% crayon::green$bold(paste(res)))
+      } else  {
+        cat("RESULTADO = " %+% crayon::red$bold(paste(res)))
+      }
+
+    }
+  )
+)
+
+
+
