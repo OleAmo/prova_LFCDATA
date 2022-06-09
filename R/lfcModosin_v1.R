184b9573a2b594aafd05e91656cd7485985f34e2
Oleguer Amorós Mèlich <o.amoros@creaf.uab.es>
2022-06-09 16:48:38 +0200

    modificated Methode DESCRIVE_TABLE and function MODOSIN_DESCRIBE_TABLE_CAT and created new Mothode DESCRIVE_VAR and created new function MODOSIN_DESCRIVE_vAR

--- R/lfcModosin_v1.R
+++ R/lfcModosin_v1.R
@@ -19,11 +19,18 @@ lfcMODOSIN <- R6::R6Class(
     #      .) DESCRIVE_TABLE
     #      .) DESCRIVE_VALUE
 
+    # ... GET_DATA....
+    # ................
+
     get_data = function(table_name){
       res <- private$data_cache[[glue::glue("{table_name}")]] %||%
         { super$get_data(table_name) }
       return(res)
     },
+
+    # ... AVAIL TABLES ...
+    # ....................
+
     avail_tables = function() {
       # no tables = Tablas que no se mostraran
       no_tables<- c("geography_columns","geometry_columns","layer","spatial_ref_sys","topology")
@@ -35,6 +42,10 @@ lfcMODOSIN <- R6::R6Class(
       res_2 <- res_1[!res_1 %in% no_tables]
       return(res_2)
     },
+
+    # ... DESCRIVE TABLE ...
+    # ......................
+
     describe_table = function(tables){
       tables_dict <- nfi_table_dictionary()
       variables_thes <- suppressMessages(self$get_data('variables_thesaurus'))
@@ -45,7 +56,23 @@ lfcMODOSIN <- R6::R6Class(
           tables_dict = tables_dict, variables_thes = variables_thes
         )
       return(invisible(self))
+    },
 
+    # ... DESCRIVE VAR ...
+    # ....................
+
+    describe_var = function(variables) {
+
+      variables_thes <- suppressMessages(self$get_data('variables_thesaurus'))
+      numerical_thes <- suppressMessages(self$get_data('variables_numerical'))
+
+      variables %>%
+        purrr::map(
+          modosin_describe_var_eng,
+          variables_thes = variables_thes, numerical_thes = numerical_thes
+        )
+
+      invisible(self)
 
     }
 
--- R/utils_Local_Host.R
+++ R/utils_Local_Host.R
@@ -1,4 +1,8 @@
 
+# ........... DESCRIBRE TABLE ...............
+# ...........................................
+
+
 
 modosin_describe_table_cat = function(table, tables_dict, variables_thes){
   variable_names <- variables_thes %>%
@@ -27,4 +31,49 @@ modosin_describe_table_cat = function(table, tables_dict, variables_thes){
           crayon::magenta()
       )
       cat('\n')
+}
+
+# .......... DESCRIBRE VARIABLE .............
+# ...........................................
+
+
+modosin_describe_var_eng <- function(variable, variables_thes, numerical_thes) {
+
+  . <- NULL
+
+  variables_thes %>%
+    dplyr::filter(.data$var_id == variable) %>%
+    dplyr::left_join(numerical_thes, by = c("var_id", "var_table")) %>%
+    dplyr::group_by(.data$var_description_eng) %>%
+    dplyr::group_walk(
+      ~ cat(
+        "\n",
+        # var name
+        crayon::yellow$bold(glue::glue(
+          "{.x$translation_eng %>% unique()} ({.x$var_id %>% unique()})"
+        )),
+        "\n",
+        # var description
+        strwrap(crayon::green(.y$var_description_eng), width = 72),
+        "\n",
+        # var units
+        crayon::blue$bold(
+          "Units: [" %+%
+            crayon::blue$italic$bold(
+              glue::glue("{(.x$var_units %na% ' - ') %>% unique()}")
+            ) %+%
+            "]"
+        ),
+        "\n",
+        # tables present
+        "Present in the following tables:\n",
+        crayon::magenta(glue::glue_collapse(
+          glue::glue(" - {sort(.x$var_table)}"), sep = '\n'
+        )),
+        # cat options
+        sep = '', fill = 80
+      )
+    )
+
+  return(invisible(NULL))
 }
